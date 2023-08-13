2b185bd743d86ca6c7d8b51eee257d60e4e5705f
Griffin Ferguson <griffinferguson.us@gmail.com>
2023-08-13 02:01:14 -0500

    add(script): Add keyboard controls

---
+++ src/assets/scripts/controls.js
@@ -0,0 +1,85 @@
+"use strict";
+var keybinds = {
+    map: {
+        up: "ArrowUp",
+        down: "ArrowDown",
+        left: "ArrowLeft",
+        right: "ArrowRight",
+    },
+    toolbar: {
+        toggleTransport: "F1",
+        toggleExtraction: "F2",
+    },
+    cursor: {
+        fire: "Enter",
+        up: "W",
+        down: "S",
+        left: "A",
+        right: "D",
+    }
+};
+var mapStep = tileSize / 7;
+window.addEventListener("keydown", (e) => {
+    var key = e.key.length == 1 ? e.key.toUpperCase() : e.key;
+    console.log(key, e.key);
+    switch (key) {
+        case keybinds.map.right:
+            moveMap.right(mapStep);
+            break;
+        case keybinds.map.left:
+            moveMap.left(mapStep);
+            break;
+        case keybinds.map.down:
+            moveMap.down(mapStep);
+            break;
+        case keybinds.map.up:
+            moveMap.up(mapStep);
+            break;
+        case keybinds.toolbar.toggleTransport:
+            toggleBuildList(toolbarToggle[0]);
+            break;
+        case keybinds.toolbar.toggleExtraction:
+            toggleBuildList(toolbarToggle[1]);
+            break;
+        case keybinds.cursor.fire:
+            cursorMode();
+            break;
+        case keybinds.cursor.up:
+            var currentCoords = playerCoords;
+            currentCoords.y.grid -= 1;
+            if (currentCoords.y.grid < 0) {
+                currentCoords.y.grid = 7;
+                currentCoords.y.tile -= 1;
+            }
+            updatePlayerCoords(currentCoords);
+            break;
+        case keybinds.cursor.down:
+            var currentCoords = playerCoords;
+            currentCoords.y.grid += 1;
+            if (currentCoords.y.grid > 7) {
+                currentCoords.y.grid = 0;
+                currentCoords.y.tile += 1;
+            }
+            updatePlayerCoords(currentCoords);
+            break;
+        case keybinds.cursor.right:
+            var currentCoords = playerCoords;
+            currentCoords.x.grid += 1;
+            if (currentCoords.x.grid > 7) {
+                currentCoords.x.grid = 0;
+                currentCoords.x.tile += 1;
+            }
+            updatePlayerCoords(currentCoords);
+            break;
+        case keybinds.cursor.left:
+            var currentCoords = playerCoords;
+            currentCoords.x.grid -= 1;
+            if (currentCoords.x.grid < 0) {
+                currentCoords.x.grid = 7;
+                currentCoords.x.tile -= 1;
+            }
+            updatePlayerCoords(currentCoords);
+            break;
+    }
+});
+//# sourceMappingURL=controls.js.map
